6e2b8b9a2766c94bde34f16f1612b1877237e0e9
alba0gf <albagdosmil@gmail.com>
2025-07-17 11:40:28 +0200

    Update app.py

--- app.py
+++ app.py
@@ -192,20 +192,7 @@ def display_results():
             organism_counts = data['metadata']['organism'].value_counts()
             st.bar_chart(organism_counts)
             
-            # feedback
-        st.markdown("---")
-        st.subheader("💬 Help us improve")
-
-        with st.form("feedback_form"):
-            st.write("Have feedback or feature requests?")
-            feedback = st.text_area("Let us know what worked, what didn't, or what you'd like to see.", height=150)
-            submitted = st.form_submit_button("Submit Feedback")
             
-            if submitted:
-                with open("feedback_log.txt", "a", encoding="utf-8") as f:
-                    f.write(f"{feedback}\n---\n")
-                st.success("Thanks for your feedback! 🙌")
-    
     with tab2:
         st.subheader("Cleaned Metadata")
         
@@ -314,20 +301,6 @@ def display_results():
             else:
                 st.success("No missing values detected in metadata!")
                 
-                # feedback
-        st.markdown("---")
-        st.subheader("💬 Help us improve")
-
-        with st.form("feedback_form"):
-            st.write("Have feedback or feature requests?")
-            feedback = st.text_area("Let us know what worked, what didn't, or what you'd like to see.", height=150)
-            submitted = st.form_submit_button("Submit Feedback")
-            
-            if submitted:
-                with open("feedback_log.txt", "a", encoding="utf-8") as f:
-                    f.write(f"{feedback}\n---\n")
-                st.success("Thanks for your feedback! 🙌")
-            
         
         # Data quality recommendations
         st.subheader("Data Quality Recommendations")
